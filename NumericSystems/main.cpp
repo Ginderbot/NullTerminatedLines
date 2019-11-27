b015637a56600a0a82b839ad907ed73dd0cc6ea6
Student <Student@304-1>
2019-11-27 20:54:19 +0200

    +

--- NumericSystems/main.cpp
+++ NumericSystems/main.cpp
@@ -5,26 +5,36 @@ using namespace std;
 
 char* dec_to_bin(int dec);
 int bin_to_dec(char* binary);
+bool isBin(char str[]);
 
 void main()
 {
 	setlocale(LC_ALL, "");
-	int dec;
+	/*int dec;
 	cout << "Введите десятичное число: "; cin >> dec;
 	char* binary = dec_to_bin(dec);
 	cout << binary << endl;
 	cout << "My binary: " << binary << endl;
-	cout << bin_to_dec(binary);
-	delete[] binary;
+	cout <<"10 "<< bin_to_dec(binary);
+	delete[] binary;*/
+	const int n = 33;
+	char*binary = new char[n] {};
+	cout << "Введите двоичное число: "; cin >> binary;
+	cout << bin_to_dec(binary) << endl;
+
 }
 
 char* dec_to_bin(int dec)
 {
-	int n = 0;
-	[&n, dec]()
+	int n = 24;
+	/*[&n, dec](int dec)
 	{
-		for (; dec; n++)dec /= 2;
-	};
+		for (; dec; n++)
+			dec /= 2;
+	};*/
+
+	cout << n << endl;
+	cout << dec << endl;
 	char* binary = new char[n] {};
 	for (int i = 0; dec; i++, dec /= 2)binary[i] = dec % 2 + 48;
 	/*for (n--; n >= 0; n--)
@@ -39,11 +49,22 @@ char* dec_to_bin(int dec)
 	return binary;
 }
 
+bool isBin(char str[])
+{
+	for (int i = 0; str[i]; i++)
+	{
+		if (!(str[i] == '0' || str[i] == '1'))
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
 int bin_to_dec(char* binary)
 {
 	int sum = 0;
 	int plus = 0;
-	strlen(binary);
 		for (int i = strlen(binary); i >= 0; i--)
 		{
 			(binary[i] == '1') ? sum += plus : false;
